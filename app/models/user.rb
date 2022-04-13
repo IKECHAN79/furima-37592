061fef38fd7c24900408f46a850110889c6ba2b5
IKECHAN79 <saki07098@gmail.com>
2022-04-13 17:57:08 +0900

    レビュー後修正②

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname,        presence: true 
-         validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-         validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+         validates :first_name,      presence: true, format: { with: /\A[/\A[ぁ-んァ-ヶ一-龥々]/+\z }
+         validates :last_name,       presence: true, format: { with: /\A[/\A[ぁ-んァ-ヶ一-龥々]/+\z }
          validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :kana_last_name,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :birthday,        presence: true
          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-         validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+      
          
 
 
