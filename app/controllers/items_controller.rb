28684921b6f6b6207174ab9af6794c75d5af0798
IKECHAN79 <saki07098@gmail.com>
2022-04-15 17:35:15 +0900

    販売手数料・販売利益実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :item_more).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,8 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, presence: true
   validates :prefecture_id,      presence: true 
   validates :days_to_ship_id,    presence: true 
-  validates :price,              presence: true
+  validates :price,              presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+  format: { with: /\A[0-9]+\z/ }
   validates :image,              presence: true
   
   
