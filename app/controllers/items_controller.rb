9a14974389d33154d72a949f2791d60177799c76
IKECHAN79 <saki07098@gmail.com>
2022-04-16 21:18:09 +0900

    テストコード実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :item_more).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :item_more, :price,:category_id, :condition_id, :delivery_charge_id, :prefecture_id, :days_to_ship_id).merge(user_id: current_user.id)
   end
 
 end
