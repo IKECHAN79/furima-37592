49334a0ec5e0b29cc35eb3394b2ad4a484de3d0b
IKECHAN79 <saki07098@gmail.com>
2022-04-17 15:04:57 +0900

    レビューを受けて修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,17 +7,17 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, presence: true, numericality: { other_than: 1 }
   validates :prefecture_id,      presence: true, numericality: { other_than: 1 } 
   validates :days_to_ship_id,    presence: true, numericality: { other_than: 1 }
-  validates :price,              presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-  format: { with: /\A[0-9]+\z/ }
+  validates :price,              presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
   validates :image,              presence: true
   
   
   belongs_to :category
-  belongs_to :Condition
+  belongs_to :condition
   belongs_to :delivery_charge
   belongs_to :prefecture
   belongs_to :days_to_ship
   has_one_attached :image
   belongs_to :user
-  has_one    :buy
+  #has_one    :buy
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -61,7 +61,7 @@ describe '商品出品' do
     it 'priceが空では保存できない' do
       @item.price = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include("Price can't be blank", "Price is not a number", "Price is invalid")
+      expect(@item.errors.full_messages).to include("Price can't be blank", "Price is not a number")
 
     end
     it 'userが紐付いていないと保存できない' do
@@ -73,6 +73,46 @@ describe '商品出品' do
       @item.image = nil
       @item.valid?
       expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+    it 'カテゴリーに「---」が選択されている場合は出品できない' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
+    it '商品の状態に「---」が選択されている場合は出品できない' do
+      @item.condition_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition must be other than 1")
+    end
+    it '配送料の負担に「---」が選択されている場合は出品できない' do
+      @item.delivery_charge_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery charge must be other than 1")
+    end
+    it '発送元の地域に「---」が選択されている場合は出品できない' do
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+    end
+    it '発送日までの日数に「---」が選択されている場合は出品できない' do
+      @item.days_to_ship_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Days to ship must be other than 1")
+    end
+    it '価格に半角数字以外が含まれている場合は出品できない' do
+      @item.price = '１００００'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+      it '価格が300円未満では出品できない' do
+      @item.price = '100'
+      @item.valid?
+    expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+    end
+    it '価格が9_999_999円を超えると出品できない' do
+      @item.price = '10000000'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
     end
   end
 end
