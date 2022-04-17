5e05d17b680e27dd963f33d071b2ad8db48ca035
IKECHAN79 <saki07098@gmail.com>
2022-04-17 10:17:41 +0900

    エラーハンドリング実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :item_name,          presence: true
   validates :item_more,          presence: true  
-  validates :category_id,        presence: true 
-  validates :condition_id,       presence: true 
-  validates :delivery_charge_id, presence: true
-  validates :prefecture_id,      presence: true 
-  validates :days_to_ship_id,    presence: true 
+  validates :category_id,        presence: true, numericality: { other_than: 1 } 
+  validates :condition_id,       presence: true, numericality: { other_than: 1 } 
+  validates :delivery_charge_id, presence: true, numericality: { other_than: 1 }
+  validates :prefecture_id,      presence: true, numericality: { other_than: 1 } 
+  validates :days_to_ship_id,    presence: true, numericality: { other_than: 1 }
   validates :price,              presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
   format: { with: /\A[0-9]+\z/ }
   validates :image,              presence: true
