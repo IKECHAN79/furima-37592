ccb37dd7c80afa936ecc2eb1751aa08f58bc1b73
IKECHAN79 <saki07098@gmail.com>
2022-04-25 00:49:11 +0900

    テストコード実装

---
+++ spec/factories/buy_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :buy_address do
+    post_code     {"123-4567"}
+    prefecture_id {2}
+    city          {"大阪市都島区"}
+    address       {"中野町4-2-20"}
+    building_name {"柳ビル103"}
+    phone_number  {"09012345678"}
+    association :buy
+  end
+end
--- spec/models/buy_address_spec.rb
+++ spec/models/buy_address_spec.rb
@@ -1,20 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Address, type: :model do
+RSpec.describe BuyAddress, type: :model do
   before do
-    @address = FactoryBot.build(:address)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:buy_address)
+
   end
 
 describe '商品購入' do
   context '商品購入ができる場合' do
   it 'すべての項目が存在していれば保存できる' do
-    expect(@address).to be_valid
+    expect(@buy_address).to be_valid
   end
 end
 
- context '商品購入ができない場合' do
+context '商品購入ができない場合' do
   it 'post_codeが空では保存できない' do
-  @address.post_code = ''
+  @address.post_code = nil
   @address.valid?
   expect(@address.errors.full_messages).to include(" ")
 
@@ -27,5 +29,6 @@ end
   end
 
  end
+
 end
 end
